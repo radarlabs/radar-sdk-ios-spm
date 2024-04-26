f879a91b82b2e7b177463d6b483de2f17eea2b2b
Kenny Hu <kenny.hu@radar.com>
2024-04-26 11:36:47 -0400

    move binary

--- Package.swift
+++ Package.swift
@@ -2,12 +2,6 @@
 
 import PackageDescription
 
-
-let radarXCFramework = Target.binaryTarget(
-  name: "RadarSDK",
-  url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.9.14/RadarSDK.xcframework.zip",
-  checksum: "bbf22af28622881220a85b6698b0e560da49a54ad184f35509e328390ecc8517")
-
 let package = Package(
     name: "RadarSDK",
     platforms: [
@@ -15,7 +9,11 @@ let package = Package(
     ],
   products: [.library(name: "RadarSDK", targets: ["RadarSDK", "_RadarStub"])],
   targets: [
-    radarXCFramework,
     .target(name: "_RadarStub"),
+    .binaryTarget(
+        name: "RadarSDK",
+        url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.9.14/RadarSDK.xcframework.zip",
+        checksum: "bbf22af28622881220a85b6698b0e560da49a54ad184f35509e328390ecc8517"
+    ),
   ]
 )

cbb46dc22fed9181b48f6670aaf7b3ba57cbe587
KennyHuRadar <139801512+KennyHuRadar@users.noreply.github.com>
2024-06-03 11:08:04 +0000

    [create-pull-request] automated change

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     .target(name: "_RadarStub"),
     .binaryTarget(
         name: "RadarSDK",
-        url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.12.0/RadarSDK.xcframework.zip",
-        checksum: "c3d700abb9cf6a54b173e4af84ff29b7aaf211da43d9ec4c56c17ee38760897b"
+        url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.13.0/RadarSDK.xcframework.zip",
+        checksum: "39e886403b8a852bd1b1527e9c4a6ba590479b8aaf4b6a419c55195e1dc8d91d"
     ),
   ]
 )

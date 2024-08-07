516b91da57aacd1628940e33acda377b7b9b36a9
Kenny Hu <kenny.hu@radar.com>
2024-08-07 12:32:46 -0400

    add in comments to anchor sed

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
         .binaryTarget(
             name: "RadarSDK",
             url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.16.1-beta.5/RadarSDK.xcframework.zip",
-            checksum: "7cb651aeda3c9e6342098c82c8f96780089a304c0dd867504ea2368e4063137d"
+            checksum: "7cb651aeda3c9e6342098c82c8f96780089a304c0dd867504ea2368e4063137d" // RadarSDK checksum
         ),
         .binaryTarget(
             name: "RadarSDKMotion",
             url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.16.1-beta.5/RadarSDKMotion.xcframework.zip",
-            checksum: "99ca5fd17934b6c2297d808b7d7fca188293d0e9680803bdf2cd6342edd7ff4b" // Replace with the actual checksum
+            checksum: "99ca5fd17934b6c2297d808b7d7fca188293d0e9680803bdf2cd6342edd7ff4b" // RadarSDKMotion checksum
         )
     ]
 )

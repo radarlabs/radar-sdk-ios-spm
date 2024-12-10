139b39fbc0be9c8465a538fb9287aa34919ae1e9
KennyHuRadar <139801512+KennyHuRadar@users.noreply.github.com>
2024-12-10 20:29:34 +0000

    [create-pull-request] automated change

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
         .target(name: "_RadarStub"),
         .binaryTarget(
             name: "RadarSDK",
-            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.19.3/RadarSDK.xcframework.zip",
-            checksum: "86a2f5fa6ac735a12fd8c8c78339c950fd57d63fc60cef8b58e8efce9c04de00" // RadarSDK checksum
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.19.4/RadarSDK.xcframework.zip",
+            checksum: "b5dfe94af921a325ebcac100e94ecfb6ecbac8fb44b8579c84ecfb71cb9980cc" // RadarSDK checksum
         ),
         .binaryTarget(
             name: "RadarSDKMotion",
-            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.19.3/RadarSDKMotion.xcframework.zip",
-            checksum: "0967174af0932340c1d5e661ce818380ae73c73263d50e363961b00dad25ac44" // RadarSDKMotion checksum
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.19.4/RadarSDKMotion.xcframework.zip",
+            checksum: "34b9c606bee2c4b142425ae66122b61058a95a067cbcf575689e9270aed4b3a6" // RadarSDKMotion checksum
         ),
     ]
 )

7b06f4f279c807ce3c399c90cba2aea3368eb400
gvivan <gvivan6@gmail.com>
2025-08-15 14:58:52 -0400

    add RadarSDKIndoors binary target and automation support

--- Package.swift
+++ Package.swift
@@ -10,18 +10,24 @@ let package = Package(
     products: [
           .library(name: "RadarSDK", targets: ["RadarSDK", "_RadarStub"]),
           .library(name: "RadarSDKMotion", targets: ["RadarSDKMotion", "_RadarStub"]),
+          .library(name: "RadarSDKIndoors", targets: ["RadarSDKIndoors", "_RadarStub"]),
       ],
       targets: [
         .target(name: "_RadarStub"),
         .binaryTarget(
             name: "RadarSDK",
             url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.21.12/RadarSDK.xcframework.zip",
-            checksum: "1c25622fce997d31d485a80b1d5a8cbe91422ddfef26b70ee122026f8ffabf8f" // RadarSDK checksum
+            checksum: "cd1c8cb527b1582929171843b48c4484457d97c17e6f826ab556dcc7a81143ac" // RadarSDK checksum
         ),
         .binaryTarget(
             name: "RadarSDKMotion",
             url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.21.12/RadarSDKMotion.xcframework.zip",
-            checksum: "9d19a80cc7450747e9120308429db5326b8ebbdb2e2ad1dcdd837b901c4787aa" // RadarSDKMotion checksum
+            checksum: "6a78704ce2b158ad5f08cac5ce5980ea72ba4533ee83c176e089b10ad1821962" // RadarSDKMotion checksum
+        ),
+        .binaryTarget(
+            name: "RadarSDKIndoors",
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.21.12/RadarSDKIndoors.xcframework.zip",
+            checksum: "d2f0dd15b500d76f1d6d9a33ba44d8968d40638c2a24b3cb56c5fa8df79bb77c" // RadarSDKIndoors checksum
         )
     ]
 )

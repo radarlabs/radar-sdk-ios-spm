7f1002350fd7412d7e59cefd780343c34946cca6
gvivan <gvivan6@gmail.com>
2025-08-27 16:22:32 -0400

    swap checksums for RadarSDKMotion and RadarSDKIndoors

--- Package.swift
+++ Package.swift
@@ -22,12 +22,12 @@ let package = Package(
         .binaryTarget(
             name: "RadarSDKMotion",
             url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDKMotion.xcframework.zip",
-            checksum: "fa5af44e9a1c9446ed48b6a88206a3faf92768725f022e648f34804269ce9e1c" // RadarSDKMotion checksum
+            checksum: "dc82402e3692bcf18642cc139852591c02915f071e3fbbd94a1808233ee80066" // RadarSDKMotion checksum
         ),
         .binaryTarget(
             name: "RadarSDKIndoors",
             url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDKIndoors.xcframework.zip",
-            checksum: "dc82402e3692bcf18642cc139852591c02915f071e3fbbd94a1808233ee80066" // RadarSDKIndoors checksum
+            checksum: "fa5af44e9a1c9446ed48b6a88206a3faf92768725f022e648f34804269ce9e1c" // RadarSDKIndoors checksum
         )
     ]
 )

357bd36affe03e3f7d40156cc870aac8d0ff9455
gvivan <gvivan6@gmail.com>
2025-08-27 14:52:52 -0400

    update checksums

--- Package.swift
+++ Package.swift
@@ -16,18 +16,18 @@ let package = Package(
         .target(name: "_RadarStub"),
         .binaryTarget(
             name: "RadarSDK",
-            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.1/RadarSDK.xcframework.zip",
-            checksum: "cd1c8cb527b1582929171843b48c4484457d97c17e6f826ab556dcc7a81143ac" // RadarSDK checksum
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDK.xcframework.zip",
+            checksum: "22b1f66dd117455c4862bde2b4b32086a309f28a70038785204c9cb47959f452" // RadarSDK checksum
         ),
         .binaryTarget(
             name: "RadarSDKMotion",
-            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.1/RadarSDKMotion.xcframework.zip",
-            checksum: "6a78704ce2b158ad5f08cac5ce5980ea72ba4533ee83c176e089b10ad1821962" // RadarSDKMotion checksum
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDKMotion.xcframework.zip",
+            checksum: "fa5af44e9a1c9446ed48b6a88206a3faf92768725f022e648f34804269ce9e1c" // RadarSDKMotion checksum
         ),
         .binaryTarget(
             name: "RadarSDKIndoors",
-            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.1/RadarSDKIndoors.xcframework.zip",
-            checksum: "d2f0dd15b500d76f1d6d9a33ba44d8968d40638c2a24b3cb56c5fa8df79bb77c" // RadarSDKIndoors checksum
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDKIndoors.xcframework.zip",
+            checksum: "dc82402e3692bcf18642cc139852591c02915f071e3fbbd94a1808233ee80066" // RadarSDKIndoors checksum
         )
     ]
 )

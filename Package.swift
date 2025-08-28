0af2010367a9e24d38e56a28262edeabbf7cefea
Vivan Garg <103859388+gvivan@users.noreply.github.com>
2025-08-28 13:10:06 -0400

    add RadarSDKIndoors binary target and automation support (#95)
    * add RadarSDKIndoors binary target and automation support
    
    * Update RadarSDK binary targets to version 3.24.0-beta.1
    
    * update checksums
    
    * swap checksums for RadarSDKMotion and RadarSDKIndoors

--- Package.swift
+++ Package.swift
@@ -10,18 +10,24 @@ let package = Package(
     products: [
           .library(name: "RadarSDK", targets: ["RadarSDK", "_RadarStub"]),
           .library(name: "RadarSDKMotion", targets: ["RadarSDKMotion", "_RadarStub"]),
+          .library(name: "RadarSDKIndoors", targets: ["RadarSDKIndoors", "_RadarStub"]),
       ],
       targets: [
         .target(name: "_RadarStub"),
         .binaryTarget(
             name: "RadarSDK",
-            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.2/RadarSDK.xcframework.zip",
-            checksum: "cb96400f36ad2b1bb6b2539325227d53d1b5e94bfaecf4eba8abc6b6fa96b82f" // RadarSDK checksum
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDK.xcframework.zip",
+            checksum: "22b1f66dd117455c4862bde2b4b32086a309f28a70038785204c9cb47959f452" // RadarSDK checksum
         ),
         .binaryTarget(
             name: "RadarSDKMotion",
-            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.2/RadarSDKMotion.xcframework.zip",
-            checksum: "5312ff8628d9a0b4c517f4fa18b762b8fe5e3b6e57c66bb436d8773ad6558bec" // RadarSDKMotion checksum
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDKMotion.xcframework.zip",
+            checksum: "dc82402e3692bcf18642cc139852591c02915f071e3fbbd94a1808233ee80066" // RadarSDKMotion checksum
+        ),
+        .binaryTarget(
+            name: "RadarSDKIndoors",
+            url: "https://github.com/radarlabs/radar-sdk-ios/releases/download/3.24.0-beta.3/RadarSDKIndoors.xcframework.zip",
+            checksum: "fa5af44e9a1c9446ed48b6a88206a3faf92768725f022e648f34804269ce9e1c" // RadarSDKIndoors checksum
         )
     ]
 )

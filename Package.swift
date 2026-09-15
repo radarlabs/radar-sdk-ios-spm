cab52a0f61c0ff8c7dac03d4557a4fecf6645e75
Alan Charles <alan.charles@radar.com>
2026-09-15 14:12:07 -0700

    Raise minimum supported iOS version to 15 (#135)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "RadarSDK",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v15)
     ],
     products: [
           .library(name: "RadarSDK", targets: ["RadarSDK", "_RadarStub"]),
